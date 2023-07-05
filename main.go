bb31eeb91fb4579248a6bec757ca3587316d6ef4
Mateusz Kedzierski <mateusz.kedzierski96@gmail.com>
2023-07-05 13:49:36 +0200

    add benchmark

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func primeNumbers(max int) []int {
+	var primes []int
+
+	for i := 2; i < max; i++ {
+		isPrime := true
+
+		for j := 2; j <= int(math.Sqrt(float64(i))); j++ {
+			if i%j == 0 {
+				isPrime = false
+				break
+			}
+		}
+
+		if isPrime {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
+
+func main() {
+	fmt.Println(primeNumbers(20))
+}

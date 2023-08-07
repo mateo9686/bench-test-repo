eda493084ccb90120a7d87ab35b12719af7e01a3
Mateusz Kedzierski <mateusz.kedzierski96@gmail.com>
2023-08-07 22:42:01 +0200

    .

--- main_test.go
+++ main_test.go
@@ -6,7 +6,15 @@ import (
 
 var num = 1000
 
+// BenchmarkPrimeNumbers
 func BenchmarkPrimeNumbers(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		primeNumbers(num)
+	}
+	b.Error("bla")
+}
+
+func BenchmarkPrimeNumbers2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		primeNumbers(num)
 	}
